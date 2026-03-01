78d625737d88e9bdf6bb416437f10e63cf48dc25
Naomi <leronnaomi@gmail.com>
2026-03-01 11:31:48 -0500

    Spawning
    Patients can manually spawn in a random room in each hospital

--- Assets/Scripts/CodeBlueSpawner.cs
+++ Assets/Scripts/CodeBlueSpawner.cs
@@ -1,24 +1,48 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class CodeBlueSpawner : MonoBehaviour
+public class CodeBlueManager : NetworkBehaviour
 {
-    public GameObject previewPrefab;
+    //patient prefabs
+    [SerializeField] private NetworkObject hospitalOnePatient;
+    [SerializeField] private NetworkObject hospitalTwoPatient;
 
-    [ContextMenu("Spawn Preview")]
-    void Spawn()
+    //Array of spawn points for each hospital
+    [SerializeField] private Transform[] hOneRoomSpawns;
+    [SerializeField] private Transform[] hTwoRoomSpawns;
+
+    //Variables for keeping track of the current patient
+    private NetworkObject currentHOnePatient;
+    private NetworkObject currentHTwoPatient;
+
+    // TEMP TEST BUTTON
+    [ContextMenu("Trigger Code Blue")]
+    public void TriggerCodeBlue()
     {
-        Instantiate(previewPrefab, transform.position, transform.rotation);
+        if (!IsServer) return;
+
+        SpawnPatient(ref currentHOnePatient, hospitalOnePatient, hOneRoomSpawns);
+        SpawnPatient(ref currentHTwoPatient, hospitalTwoPatient, hTwoRoomSpawns);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void SpawnPatient(ref NetworkObject currentPatient, NetworkObject prefab, Transform[] spawnPoints)
     {
-        
-    }
+        // despawn the previous patient
+        if (currentPatient != null && currentPatient.IsSpawned)
+        {
+            currentPatient.Despawn(true);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //spawn in a random room
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawn = spawnPoints[randomIndex];
+        NetworkObject patient = Instantiate(prefab, spawn.position, spawn.rotation);
+
+        patient.Spawn(true);
+
+        //keep track of current patient
+        currentPatient = patient;
+
+        Debug.Log($"Code Blue spawned at {spawn.name}");
     }
 }
